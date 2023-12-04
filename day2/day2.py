846d9c19adf96ede2791c93bbcb374f13c8769b5
Eric Reiss <reiss_eric@hotmail.com>
2023-12-03 19:00:48 -0700

    Completed part 1

--- day2/day2.py
+++ day2/day2.py
@@ -2,25 +2,57 @@
 C was painful, I think I will do this one in Python
 """
 
-def part1(red: int, green: int, blue: int) -> bool:
+def part1(red: int, green: int, blue: int) -> int:
     with open("input.txt", "r") as f:
+        round = 0
+        total = 0
         for line in f:
-            # print(line)
+            round += 1
+            total += round
             line = line.strip().split(" ")
-            prev_word = ""
-            for word in line:
-                if prev_word == "Game":
-                    game = ord(word[0]) - ord('0')
-                if word[:3] == "red":
-                    print("red "+prev_word)
-                elif word[:5] == "green":
-                    print("green "+prev_word)
-                elif word[:4] == "blue":
-                    print("blue "+prev_word)
-                prev_word = word
+            
+            
+            borders = []
+            for idx in range(len(line)):
+                if line[idx][-1] == ':':
+                    borders.append(idx+1)
+                if line[idx][-1] == ';':
+                    borders.append(idx+1)
+            borders.append(len(line))
+            
+            
+            bad_round = False
+            for idx in range(len(borders)-1):
+                frag = line[borders[idx]:borders[idx+1]]
+                # print(frag)
+                prev_word = ""
+                if bad_round:
+                    break
+                for word in frag:
+                    if word[:3] == "red":
+                        if int(prev_word) > red:
+                            total -= round
+                            bad_round = True
+                            break
+                    elif word[:5] == "green":
+                        if int(prev_word) > green:
+                            total -= round
+                            bad_round = True
+                            break
+                    elif word[:4] == "blue":
+                        x = int(prev_word)
+                        if int(prev_word) > blue:
+                            total -= round
+                            bad_round = True
+                            break
+                    prev_word = word  
+                    
+                        
+        return total
+    return -1
             
     
     
 if __name__ == "__main__":
-    part1(12,13,14)
+    print(part1(12,13,14))
     #part2()
